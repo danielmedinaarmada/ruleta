e1d303e110b884222e28ed2eade348c57d3243d8
Daniel Medina <daniel@medinaarmada.com>
2019-10-18 16:37:54 -0400

    Esqueleto y WinWheel

--- js/ruleta.js
+++ js/ruleta.js
@@ -1,63 +1,50 @@
 
-class Juego {
-  constructor(){
-    this.inicializar()
+let theWheel = new Winwheel({
+  'canvasId'    : 'WinWheel',
+  'outerRadius' : 200,
+  'numSegments' : 5,
+  'segments'    :[
+    { 'fillStyle' : '#FFFF00', 'text' : 'Pregunta 1' },
+    { 'fillStyle' : '#6600FF', 'text' : 'Pregunta 2' },
+    { 'fillStyle' : '#FF6600', 'text' : 'Pregunta 3' },
+    { 'fillStyle' : '#66CC00', 'text' : 'Pregunta 4' },
+    { 'fillStyle' : '#CC3300', 'text' : 'Pregunta 5' }
+  ],
+  'pins' : {
+    'number' : 15
+  },
+  'animation': {
+    'type'          : 'spinToStop',
+    'duration'      : 5,
+    'spins'         : 5
+    //'callbackSound' : playSound,
   }
+});
 
-  inicializar(){
-    this.Mensaje = this.Mensaje.bind(this)
-    this.dibujarIndicador = this.dibujarIndicador.bind(this)
-    this.segments = [ // Datos de los segmentos
-      { 'fillStyle': '#f1c40f', 'text': 'CSharp' },
-      { 'fillStyle': '#2ecc71', 'text': 'JavaScript' },
-      { 'fillStyle': '#e67e22', 'text': 'Python' },
-      { 'fillStyle': '#e74c3c', 'text': 'Php' },
-      { 'fillStyle': '#8e44ad', 'text': 'Go' },
-    ];
+/*
+let handImagen = new Image()
 
-    this.miRuleta = new Winwheel({
-      'numSegments': 5, // Número de segmentos
-      'outerRadius' : 170, // Radio externo
-      'segments': this.segments ,
-      'animation': {
-      'type': 'spinToStop', // Giro y alto
-      'duration': 5, // Duración de giro
-      'callbackFinished': 'Mensaje()', // Función para mostrar mensaje
-      'callbackAfter': 'dibujarIndicador()' // Funciona de pintar indicador
-      }
-    });
+handImagen.onload = function(){
+  let canvas = document.getElementById('WinWheel')
+  let ctx = canvas.getContext('2d')
 
-    this.miRuleta.startAnimation();
+  if (ctx){
+    ctx.save()
+    ctx.translate(200, 150);
+    ctx.rotate(theWheel.degToRad(-5))
+    ctx.translate(-200, -150)
+    ctx.drawImage(handImagen, 200, 15)
+    ctx.restore()
   }
+};
 
-  Mensaje() {
-    var SegmentoSeleccionado = this.miRuleta.getIndicatedSegment();
-    alert("Elemento seleccionado:" + SegmentoSeleccionado.text + "!");
-    this.miRuleta.stopAnimation(false);
-    this.miRuleta.rotationAngle = 0;
-    this.miRuleta.draw();
-    dibujarIndicador();
-  }
+handImagen.src = "../img/basic_pointer.png"
 
-  dibujarIndicador(){
-    var ctx = this.miRuleta.ctx;
-    ctx.strokeStyle = 'navy';
-    ctx.fillStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.moveTo(170, 0);
-    ctx.lineTo(230, 0);
-    ctx.lineTo(200, 40);
-    ctx.lineTo(171, 0);
-    ctx.stroke();
-    ctx.fill();
-  }
+let audio = new Audio("../audios/tick.mp3");
 
-
+function playSound(){
+  audio.pause()
+  audio.currentTime = 0
+  audio.play()
 }
-
-
-function empezarJuego(){
-  var juego = new Juego();
-}
-
+*/

a8d30540506d7a7004fcc4291a240235029b9c22
Daniel Medina <daniel@medinaarmada.com>
2019-10-19 17:59:16 -0400

    Instrucciones

--- js/ruleta.js
+++ js/ruleta.js
@@ -1,50 +1,49 @@
-
-let theWheel = new Winwheel({
-  'canvasId'    : 'WinWheel',
-  'outerRadius' : 200,
-  'numSegments' : 5,
-  'segments'    :[
-    { 'fillStyle' : '#FFFF00', 'text' : 'Pregunta 1' },
-    { 'fillStyle' : '#6600FF', 'text' : 'Pregunta 2' },
-    { 'fillStyle' : '#FF6600', 'text' : 'Pregunta 3' },
-    { 'fillStyle' : '#66CC00', 'text' : 'Pregunta 4' },
-    { 'fillStyle' : '#CC3300', 'text' : 'Pregunta 5' }
-  ],
-  'pins' : {
-    'number' : 15
-  },
-  'animation': {
-    'type'          : 'spinToStop',
-    'duration'      : 5,
-    'spins'         : 5
-    //'callbackSound' : playSound,
+class Juego {
+  constructor(){
+    this.divAyuda = document.getElementById('ayuda');
+    debugger
+    this.inicializar()
+    this.toggleEmpezar()
   }
-});
 
-/*
-let handImagen = new Image()
-
-handImagen.onload = function(){
-  let canvas = document.getElementById('WinWheel')
-  let ctx = canvas.getContext('2d')
-
-  if (ctx){
-    ctx.save()
-    ctx.translate(200, 150);
-    ctx.rotate(theWheel.degToRad(-5))
-    ctx.translate(-200, -150)
-    ctx.drawImage(handImagen, 200, 15)
-    ctx.restore()
+  inicializar(){
+    this.theWheel = new Winwheel({
+      'canvasId'      : 'WinWheel',
+      'numSegments'   : 5,
+      'outerRadius'   : 200,
+      'segments'      : [
+        { 'fillStyle' : '#FFFF00', 'text' : 'Pregunta 1' },
+        { 'fillStyle' : '#6600FF', 'text' : 'Pregunta 2' },
+        { 'fillStyle' : '#FF6600', 'text' : 'Pregunta 3' },
+        { 'fillStyle' : '#66CC00', 'text' : 'Pregunta 4' },
+        { 'fillStyle' : '#CC3300', 'text' : 'Pregunta 5' }
+      ],
+      'pins' : {
+        'number' : 15
+      },
+      'animation': {
+        'type'          : 'SpinToStop',
+        'duration'      :  5,
+        'spins'         :  5
+      }
+    })
+this
+    this.theWheel.startAnimation()
   }
-};
+  
 
-handImagen.src = "../img/basic_pointer.png"
-
-let audio = new Audio("../audios/tick.mp3");
-
-function playSound(){
-  audio.pause()
-  audio.currentTime = 0
-  audio.play()
+  toggleEmpezar(){
+    if(this.divAyuda.classList.contains('hide')){
+      this.divAyuda.classList.remove('hide')
+    }else{
+      this.divAyuda.classList.add('hide')
+    }
+  }
 }
-*/
+
+
+
+function empezarJuego(){
+
+  var juego = new Juego()
+}

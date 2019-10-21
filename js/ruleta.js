22fcef789456e165a16c0c233acc19dd23f3accd
Daniel Medina <daniel@medinaarmada.com>
2019-10-21 14:31:50 -0400

    Corre la rulta

--- js/ruleta.js
+++ js/ruleta.js
@@ -1,9 +1,14 @@
 class Juego {
+
   constructor(){
-    this.divAyuda = document.getElementById('ayuda');
-    debugger
+    this.divAyuda = document.getElementById('ayuda')
+    this.divPreguntas = document.getElementById('preguntas')
+    this.divPuntos = document.getElementById('puntos')
     this.inicializar()
-    this.toggleEmpezar()
+    this.toggleDivAyuda()
+    this.toggleDivPreguntas()
+    this.toggleDivPuntos()
+    this.girar()
   }
 
   inicializar(){
@@ -18,32 +23,50 @@ class Juego {
         { 'fillStyle' : '#66CC00', 'text' : 'Pregunta 4' },
         { 'fillStyle' : '#CC3300', 'text' : 'Pregunta 5' }
       ],
-      'pins' : {
-        'number' : 15
-      },
       'animation': {
-        'type'          : 'SpinToStop',
+        'type'          : 'spinToStop',
         'duration'      :  5,
         'spins'         :  5
+      },
+      'pins' : {
+        'number' : 15
       }
     })
-this
-    this.theWheel.startAnimation()
   }
   
 
-  toggleEmpezar(){
+  toggleDivAyuda(){
     if(this.divAyuda.classList.contains('hide')){
       this.divAyuda.classList.remove('hide')
     }else{
       this.divAyuda.classList.add('hide')
     }
   }
+
+  toggleDivPreguntas(){
+    if(this.divPreguntas.classList.contains('hide')){
+      this.divPreguntas.classList.remove('hide')
+    }else{
+      this.divPreguntas.classList.add('hide')
+    }
+  }
+
+  toggleDivPuntos(){
+    if(this.divPuntos.classList.contains('hide')){
+      this.divPuntos.classList.remove('hide')
+    }else{
+      this.divPuntos.classList.add('hide')
+    }
+  }
+
+  girar(){
+    this.theWheel.startAnimation()
+  }
+
 }
 
 
 
 function empezarJuego(){
-
   var juego = new Juego()
 }
